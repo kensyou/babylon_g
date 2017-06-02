2ede0a4d2bdb2abf7290f0bf977270fdc136be5e
Ken Wong CBRE <ken.wong@cbre.co.jp>
2017-06-02 17:47:02 +0900

    Added floor number

--- src/app/building3d/building3d.component.ts
+++ src/app/building3d/building3d.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, HostListener, ChangeDetectionStrategy } from '@angular/core';
 import * as BABYLON from 'babylonjs';
 import { ResizeService } from '../services/resize.service';
 import { DataService } from '../services/data.service';
 import { IBuilding } from '../services/stackingPlanModels';
 import * as filter from "lodash/fp/filter";
-
+import * as sortBy from "lodash/fp/sortBy";
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'cb-building3d',
   templateUrl: './building3d.component.html',
   styleUrls: ['./building3d.component.scss']
@@ -15,18 +18,20 @@ export class Building3dComponent implements OnInit, AfterViewInit {
   @ViewChild('renderCanvas') el: ElementRef;
 
   private _canvas: HTMLCanvasElement;
-  private _engine: BABYLON.Engine;
-  private _scene: BABYLON.Scene;
-  private _camera: BABYLON.FreeCamera;
-  private _light: BABYLON.Light;
+
+  private buildingName$: Observable<string>;
+  private buildingNameSubject: Subject<string>;
 
   constructor(private resizeService: ResizeService, private dataService: DataService) { }
 
   ngOnInit() {
+    this.buildingNameSubject = new Subject<string>();
+    this.buildingName$ = this.buildingNameSubject.asObservable();
   }
 
   ngAfterViewInit() {
     this.dataService.getBuildingData$().subscribe((building: IBuilding) => {
+      this.buildingNameSubject.next(building.buildingName);
       this._canvas = this.el.nativeElement;
       let g = new Building3DGenerator(this._canvas, this.resizeService);
       g.createScene(building);
@@ -54,7 +59,7 @@ class Building3DGenerator {
     this._scene.clearColor = new BABYLON.Color4(0, 0, 0, 0.0000000000000001);
 
     // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
-    this._camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(-8, 7.5, -18), this._scene);
+    this._camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(-8, 4.5, -18), this._scene);
 
     // target the camera to scene origin
     //this._camera.setTarget(BABYLON.Vector3.Zero());
@@ -80,18 +85,54 @@ class Building3DGenerator {
       { width: 6, height: 6, subdivisions: 2 }, this._scene);
     let groundMaterial = new BABYLON.StandardMaterial("groundTexture", this._scene);
     groundMaterial.diffuseColor = BABYLON.Color3.FromHexString("#d3d3d3");//("#67b52f");
+    groundMaterial.alpha = 0.5;
     ground.material = groundMaterial;
 
 
+
     var materialFloorTexture1 = new BABYLON.StandardMaterial("floorTexture1", this._scene);
-    materialFloorTexture1.diffuseColor = BABYLON.Color3.FromHexString("#b0ff77");
+    //materialFloorTexture1.diffuseColor = BABYLON.Color3.FromHexString("#b0ff77");
     materialFloorTexture1.backFaceCulling = false;
     materialFloorTexture1.alpha = 0.08
 
-    building.floors.forEach((f, index) => {
+    var materialFloorTexture2 = new BABYLON.StandardMaterial("floorTexture2", this._scene);
+    materialFloorTexture2.diffuseColor = BABYLON.Color3.FromHexString("#b0ff77");
+    materialFloorTexture2.backFaceCulling = false;
+    materialFloorTexture2.alpha = 0.3
+
+    let sortedFloor = sortBy(fo => (fo.floorSortOrder + 0.0001 * (fo.floorSize || fo.estimatedFloorSize || 0)), building.floors)
+    sortedFloor.forEach((f, index) => {
+      if (!f.floorSortOrder) return;
       let fbox = BABYLON.MeshBuilder.CreateBox(`box_${index}`, { width: 5, depth: 5, height: 1 / 3 }, this._scene);
-      fbox.material = materialFloorTexture1;
-      fbox.position.y = index / 3;
+      fbox.material = (f.floorSortOrder == 17 || f.floorSortOrder == 18) ? materialFloorTexture2 : materialFloorTexture1;
+      fbox.position.y = (f.floorSortOrder > 0) ?
+        (f.floorSortOrder - 1) / 3 + ((1 / 3) / 2) :
+        (f.floorSortOrder + 1) / 3 - ((1 / 3) / 2);
+      var sprite = this.makeFloorNumberLabel(this._scene, f.floorName);
+      var label = sprite.object;
+      var labelWidth = 2.8;
+      var labelHeight = sprite.height;
+
+      label.size = labelWidth;
+      label.position.x = fbox.position.x - 3.5;
+      label.position.y = fbox.position.y - (1 / 6);
+      label.position.z = 0;
+
+      // Draw a line frop label to object
+      var myLines = BABYLON.Mesh.CreateLines(`fropline_${index}`, [
+
+        // try this line, because of the offset, the line and the label rotate on different planes.
+        //new BABYLON.Vector3(label.position.x, label.position.y + 1, label.position.z),
+
+        // With this line they rotate together but the line points (obviously) at the center of the texture
+        label.position,
+        fbox.getAbsolutePosition().add(new BABYLON.Vector3(0, -(1 / 6), 0))
+      ], this._scene);
+
+      // Red
+      myLines.color = new BABYLON.Color3(0, 0, 0);
+      myLines.alpha = 0.05;
+
     });
 
     var a = 1;
@@ -144,5 +185,76 @@ class Building3DGenerator {
     });
     this.resizeService.onResize$.subscribe(() => this._engine.resize());
 
+  }
+
+
+  // Just a text-wrapping function
+  wrapText(context, text, x, y, maxWidth, lineHeight) {
+    var words = text.split(' ');
+    var line = '';
+    var numberOfLines = 0;
+
+    for (var n = 0; n < words.length; n++) {
+      var testLine = line + words[n] + ' ';
+      var metrics = context.measureText(testLine);
+      var testWidth = metrics.width;
+
+      if (testWidth > maxWidth && n > 0) {
+        context.fillText(line, x, y);
+        line = words[n] + ' ';
+        y += lineHeight;
+        numberOfLines++;
+      }
+      else {
+        line = testLine;
+      }
+    }
+    context.fillText(line, x, y);
+
+    return numberOfLines;
+  }
+  makeFloorNumberLabel(scene: BABYLON.Scene, floorNumber: string) {
+    let dynamicTexture = new BABYLON.DynamicTexture(`floorDynamicTexture_${floorNumber}`, 512, this._scene, false);
+    dynamicTexture.hasAlpha = false;
+
+    var textureContext = <any>dynamicTexture.getContext();
+    textureContext.save();
+    textureContext.textAlign = "center";
+    textureContext.font = "40px Calibri";
+
+    // Some magic numbers
+    var lineHeight = 40;
+    var lineWidth = 60;
+    var fontHeight = 30;
+    var offset = 5; // space between top/bottom borders and actual text
+    var text = floorNumber; // Text to display
+
+    var numberOfLines = 1; // I usually calculate that but for this exmaple let's just say it's 1
+    var textHeight = fontHeight + offset;
+    var labelHeight = numberOfLines * lineHeight + (2 * offset) + 20;
+
+    // Background for debug
+    // textureContext.fillStyle = "red";
+    // textureContext.fillRect(dynamicTexture.getSize().width/2, dynamicTexture.getSize().height/2 -lineHeight, lineWidth, labelHeight);
+
+    // text
+    textureContext.fillStyle = "black";
+    this.wrapText(textureContext, text, dynamicTexture.getSize().width / 2 + lineWidth / 2, dynamicTexture.getSize().height / 2 - lineHeight + textHeight, lineWidth, lineHeight);
+    textureContext.restore();
+
+    dynamicTexture.update(false);
+
+    // Create the sprite
+    var spriteManager = <any>new BABYLON.SpriteManager(`sm_${floorNumber}`, "", 2, 512, scene);
+    spriteManager._spriteTexture = dynamicTexture;
+    spriteManager._spriteTexture.wrapU = BABYLON.Texture.CLAMP_ADDRESSMODE;
+    spriteManager._spriteTexture.wrapV = BABYLON.Texture.CLAMP_ADDRESSMODE;
+    var sprite = new BABYLON.Sprite(`textSprite_${floorNumber}`, spriteManager);
+
+    var result = {
+      object: sprite,
+      height: labelHeight
+    };
+    return result;
   }
 }
